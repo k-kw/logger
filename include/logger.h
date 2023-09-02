abbc4417e4281acb8f708110474a301d85b05dab
k-kw <kouki1212@chiba-u.jp>
2023-09-02 19:21:41 +0900

    add destructor, default move operator and constructor, delete copy operator and constructor, add prepend time-stamp, add log csv format

--- include/logger.h
+++ include/logger.h
@@ -5,6 +5,10 @@
 #include <sstream>
 #include <iomanip>
 #include <fstream>
+// #include <filesystem>
+#include <vector>
+
+// namespace fs = std::filesystem;
 
 class Logger {
 private:
@@ -12,13 +16,31 @@ private:
     std::string logdir;
     std::chrono::high_resolution_clock::time_point time_1;
     std::chrono::high_resolution_clock::time_point time_2;
-
     std::ofstream fp;
 
 public:
+    // constructor
     Logger(const std::string& logdir, const std::string& logfile);
+    // destructor
+    ~Logger();
+
+    // prohibition of copy constructor
+    Logger(const Logger&)=delete;
+    // prohibition of copy operator
+    Logger& operator=(const Logger&)=delete;
+
+    // allow of move constructor
+    Logger(Logger&&)=default;
+    // allow of move operator
+    Logger& operator=(Logger&&)=default;
+
     void set_logfile(const std::string& logfile);
     void set_logdir(const std::string& logdir);
+
+    // bool check_exist();
+    std::string prepend_timestamp(const std::string& message);
+
+    void logtext(const std::string& message, const bool timestamp_flg);
     void logtext(const std::string& message);
     template <typename T>
     void lognumber(const std::string& prefix, T number, const std::string& suffix, int precision);
@@ -26,6 +48,9 @@ public:
     void set_time_1();
     void set_time_2();
     void loglaptime(const std::string& prefix, const std::string& suffix, int precision);
+
+    template <class... Args>
+    void logcsvformat(const Args&... msgs);
 
     void openfp();
     void closefp();
@@ -36,4 +61,19 @@ void Logger::lognumber(const std::string& prefix, T number, const std::string& s
     std::ostringstream oss;
     oss << std::fixed << std::setprecision(precision) << number;
     logtext(prefix + oss.str() + suffix);
+}
+
+template <class... Args>
+void Logger::logcsvformat(const Args&... msgs){
+    std::string tmp;
+    std::initializer_list<std::string> inputs{msgs...};
+    int list_pnt=0;
+    for (auto msg : inputs){
+        if (list_pnt >= inputs.size()-1) msg+="\n";
+        else msg+=",";
+        tmp+=msg;
+        list_pnt++;
+    }
+
+    this->logtext(tmp);
 }
--- logger.cpp
+++ logger.cpp
@@ -1,12 +1,21 @@
 #include "logger.h"
 #include <fstream>
 #include <iostream>
+// #include <filesystem>
 #include <string>
+#include <ctime>
+
+// namespace fs = std::filesystem;
 
 std::string msg_prefix = "Logger: ";
 
 Logger::Logger(const std::string& logdir, const std::string& logfile)
     : logfile(logfile), logdir(logdir) {}
+
+Logger::~Logger(){
+    // std::cout<<"Logger destructor\n";
+    if (fp.is_open()) fp.close();
+}
 
 void Logger::set_logfile(const std::string& logfile) {
     this->logfile = logfile;
@@ -14,6 +23,37 @@ void Logger::set_logfile(const std::string& logfile) {
 
 void Logger::set_logdir(const std::string& logdir) {
     this->logdir = logdir;
+}
+
+// bool Logger::check_exist(){
+//     return fs::is_regular_file(logdir+"/"+logfile);
+// }
+
+std::string Logger::prepend_timestamp(const std::string& message){
+    std::chrono::system_clock::time_point now_p=std::chrono::system_clock::now();
+    std::time_t now_t=std::chrono::system_clock::to_time_t(now_p);
+    std::string now_str=std::ctime(&now_t);
+    now_str.resize(now_str.size()-1);
+    std::string time_message=now_str+": "+message;
+    return time_message;
+}
+
+void Logger::logtext(const std::string& message, const bool timestamp_flg) {
+    std::string message_tmp;
+    if (timestamp_flg) message_tmp=prepend_timestamp(message);
+    else message_tmp=message;
+
+    if (fp.is_open()) {
+        fp << message_tmp;
+    } else {
+        fp.open(logdir + "/" + logfile, std::ios::app);
+        if (!fp.is_open()){
+            std::cout<< msg_prefix + "Log file can not be opend.\n";
+        }else{
+            fp << message_tmp;
+            fp.close();
+        }
+    }
 }
 
 void Logger::logtext(const std::string& message) {
@@ -55,10 +95,6 @@ void Logger::openfp(){
 }
 
 void Logger::closefp(){
-    if (fp.is_open()){
-        fp.close();
-    }
-    else{
-        std::cout<< msg_prefix + "Log file is already closed.\n";
-    }
+    if (fp.is_open()) fp.close();
+    else std::cout<< msg_prefix + "Log file is already closed.\n";
 }
